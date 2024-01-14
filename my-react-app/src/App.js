fde86ac212be1debe3e29f0b07fcb5cce486d36f
Paul Sorenson <paul.sorenson@strivr.com>
2024-01-14 11:30:16 -0700

    simple frontend auth

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -2,11 +2,15 @@ import React, { useState } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import WebRequests from './WebRequests';
 import ViewsContainer from './views/ViewsContainer';
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+import { useAuth0 } from "@auth0/auth0-react";
 
 export default function App() {
   const [excerciseOptions, setExcerciseOptions] = useState(null);
   const [users, setUsers] = useState(null);
   const[excerciseRecord, setExcerciseRecord] = useState(null);
+  const { isAuthenticated } = useAuth0();
 
   function HandleListLoaded(excercises) {
     setExcerciseOptions(excercises);
@@ -16,10 +20,19 @@ export default function App() {
     setUsers(users);
   };
 
-  return (
-    <div className="container">
-        <div><ViewsContainer excerciseOptions={excerciseOptions} users={users} onExcerciseRecordAdded={setExcerciseRecord}/></div>
-        <div><WebRequests OnFetchedUsers={HandleUsersLoaded} OnFetchedExcercises={HandleListLoaded} excerciseRecord={excerciseRecord}/></div>
-    </div>
-  );
+  if(!isAuthenticated) {
+    return (
+      <div className="container">
+      <LoginButton/>
+      </div>
+    );
+  } else {
+    return (
+      <div className="container">
+          <LogoutButton/>
+          <div><ViewsContainer excerciseOptions={excerciseOptions} users={users} onExcerciseRecordAdded={setExcerciseRecord}/></div>
+          <div><WebRequests OnFetchedUsers={HandleUsersLoaded} OnFetchedExcercises={HandleListLoaded} excerciseRecord={excerciseRecord}/></div>
+      </div>
+    );
+  }
 }
--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,7 +8,15 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain={process.env.REACT_APP_AUTH0_DOMAIN}
+      clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
