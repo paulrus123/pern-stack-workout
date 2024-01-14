e040c0decd50bb8fa3cd4dfdc79c7c3b01b585da
Paul Sorenson <paul.sorenson@strivr.com>
2024-01-14 12:27:15 -0700

    bugfix

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -13,7 +13,7 @@ export default function App() {
 
   useEffect(() => {
     const updateUserProfileAndLoadPage = async () => {
-      await updateUserProfileInDB(user);
+      await putUser(user);
       await fetchCurrentUsers((users) => {
         setUsers(users);
       });
@@ -23,7 +23,7 @@ export default function App() {
     };
 
     if (isAuthenticated) {
-      putUser(user);
+      updateUserProfileAndLoadPage();
     }
   }, [isAuthenticated, user]);
 
